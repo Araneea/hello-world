f9fee80ff4ef38b519b24dcb56d02a006a74bf69
Mateusz Ciochon <z1146566@s0053-07.stud.matinf.uj.edu.pl>
2017-10-26 09:14:33 +0200

    nowy plik

---
+++ costam.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+
+int main()
+{
+int tab[10];
+	//srand(time(null));
+	for(int i=0;i<10;i++) tab[i]=rand()%10;
+	
+	for(int i=0;i<10;i++) cout<<tab[i]<<endl;
+	cout<<"max tab "<<max(tab,10);
+	cout<<"min tab "<<min(tab,10);
+
+
+
+}
